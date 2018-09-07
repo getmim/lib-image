40ac972d1bf39cf3c5aa079d34db780883c9cfb2
Iqbal Fauzi <iqbalfawz@gmail.com>
2018-09-07 12:20:14 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# lib-image
+
+Adalah module penyedia class `claviska/SimpleImage`. Module ini hanya bertugas
+menyediakan class `\claviska\SimpleImage`. Silahkan mengacu pada projek tersebut
+untuk cara penggunaan.
+
+## Instalasi
+
+Jalankan perintah di bawah di folder aplikasi:
+
+```
+mim app install lib-image
+```
+
+## Lisensi
+
+Module ini menggunakan library [claviska/SimpleImage](https://github.com/claviska/SimpleImage),
+silahkan mengacup pada library tersebut untuk lisensi.
---
+++ modules/lib-image/config.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    '__name' => 'lib-image',
+    '__version' => '0.0.1',
+    '__git' => 'git@github.com:getmim/lib-image.git',
+    '__license' => 'MIT',
+    '__author' => [
+        'name' => 'Iqbal Fauzi',
+        'email' => 'iqbalfawz@gmail.com',
+        'website' => 'http://iqbalfn.com/'
+    ],
+    '__files' => [
+        'modules/lib-image' => ['install','update','remove']
+    ],
+    '__dependencies' => [
+        'required' => [],
+        'optional' => []
+    ],
+    'autoload' => [
+        'classes' => [
+            'claviska\\SimpleImage' => [
+                'type' => 'file',
+                'base' => 'modules/lib-image/third-party/SimpleImage.php'
+            ]
+        ],
+        'files' => []
+    ]
+];
