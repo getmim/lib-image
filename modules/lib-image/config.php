ca347ab3b4e32b3e38209504ab694e8454dfacf9
Iqbal Fauzi <iqbalfawz@gmail.com>
2021-03-07 23:00:42 +0700

    Update lib 2021-03-07

--- modules/lib-image/config.php
+++ modules/lib-image/config.php
@@ -2,7 +2,7 @@
 
 return [
     '__name' => 'lib-image',
-    '__version' => '0.0.2',
+    '__version' => '0.1.0',
     '__git' => 'git@github.com:getmim/lib-image.git',
     '__license' => 'MIT',
     '__author' => [
